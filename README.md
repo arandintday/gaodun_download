3c0b352c8f21f25e6aed7d64e9aa0bd6ff7d1ccd
arandintday <lzchappytime2@gmail.com>
2020-12-29 02:42:19 +0800

    update

--- README.md
+++ README.md
@@ -1,22 +1,35 @@
+# 介绍
+
+下载并合并高顿视频，魔改自jiangph1001的gaodun_download，增加了Windows支持
 
 
+# 依赖
 
+```
+pip3 install pycryptodome
+```
 
 # 运行
+
+按F12打开chrome的开发者模式，找到Network选项，然后刷新视频播放的页面，下面会出现一些记录
+
+右键选择任意一条记录，找到`Save all as HAR with content`，保存到程序目录下的`HAR`文件夹中，例如此处命名为`test.har`
+
+然后执行命令即可开始下载
 
 ```
 python3 download.py
 ```
 
-执行后会自动通过记事本打开`video.txt`文件，此时手动将m3u8信息写入video.txt即可（覆盖已有的就行）
-
-**目前仅支持windows下运行**
-
-m3u8信息目前需要手动获取，通过chrome的开发者模式找到，样例如`video.txt`
 
 
+# 注意
 
-# 原理
-
-下载多个ts视频流，下载完成后统一执行合并，默认视频都是AES-128加密的，所以还需要解密
-
+- har目录下**不要放**不是har的文件
+- 文件名必须以`.har`结尾
+- 最后生成的视频文件名与har文件名和时间有关，例如
+  - 保存的是`test.har`
+  - 最后生成的视频文件是`test.ts`
+- 可以往har文件夹中可以放入尽可能多的har文件，会依次下载
+- 下载成功后，har文件会自动删除
+- 已知bug：部分har文件cookie格式较为特殊，导致本脚本无法识别出key值，等待原作者更新算法
---
+++ download_win.py
@@ -0,0 +1,90 @@
+# -*- coding:utf-8 -*-  
+import os
+import requests,time
+from Crypto.Cipher import AES
+from binascii import b2a_hex, a2b_hex
+#import multiprocessing
+import uuid,json
+import base64
+import time,random
+def parse_har(har_file_name):
+    file = open(har_file_name,'r',encoding='utf-8')
+    content = file.read()
+    my_har_origin = json.loads(content)
+    print("[+] Json decoding successfully")
+    m3u8 = None
+    IV = None
+    for req in my_har_origin['log']['entries']:
+        #req_json = json.loads(req)
+        if 'request' in req:
+            if 'url' in req['request']:
+                if "src/get?id=" in req['request']['url']:
+                    m3u8 = req['response']['content']['text']
+                if "authorize?id=" in req['request']['url']:
+                    IV = req['response']['content']['text']
+                    #print(req['response']['content']['text'])
+    file.close()
+    print("[+] Get m3u8 and key successfully")
+    return m3u8,IV
+
+def read_m3u8(m3u8,key,filename):
+    all_content = base64.b64decode(m3u8).decode('utf-8')
+    key  = base64.b64decode(key)
+    print("[+] Base64 decoder load successfully")
+    cryptor = AES.new(key, AES.MODE_CBC, key) 
+    if "#EXTM3U" not in all_content:
+        print("[-] E: Not found video in " + filename)
+        return
+    print("[+] Decrypt video successfully")
+    file_line = all_content.split("\n")
+    #time_str = time.strftime('%d-%H%M%S', time.localtime(time.time()))
+    ts_name = "download//{}.ts".format(filename)
+    writer = open(ts_name, 'wb')
+    print("[*] Downloading ts file...")
+    num=1
+    max_num=int(int(len(file_line))/2-4)
+    for index, line in enumerate(file_line): # 第二层 
+        if "EXTINF" in line: # 找ts地址并下载
+            pd_url = file_line[index + 1] # 拼出ts片段的URL
+            while True:
+                try:
+                    res = requests.get(pd_url)
+                    #print(filename,pd_url)
+                    break
+                except Exception as e:
+                    #print(e)
+                    print("[-] E: Rertrying...")
+                    time.sleep(random.randint(1,5))
+            writer.write(cryptor.decrypt(res.content))
+            perc=int((num/(max_num))*50)
+            #print(" [",end="")
+            #for i in range(0,perc):
+                #print("=",end="")
+            #for j in range(0,50-perc):
+                #print(" ",end="")
+            #print("]"+str(num)+" of "+str(max_num),end="\r")
+            print("[*] "+str(num)+" of "+str(max_num))
+            #print("{} finish download".format(filename))
+            num+=1
+    writer.close()
+    os.system('del har//'+har)
+    print("[*] Download successful")
+    return 0
+
+if __name__ == '__main__': 
+    har_list = os.listdir('har')
+    os.system("mkdir download")
+    print("[*] Download queue:"+str(har_list))
+    #pool = multiprocessing.Pool(processes = 1)
+    for har in har_list:
+        file_without_suffix = har.rsplit('.')[0]
+        print("[*] Now processing:"+file_without_suffix)
+        try:
+            m3u8,key = parse_har('har//'+har)
+            read_m3u8(m3u8,key,file_without_suffix)
+        except Exception as e:
+            print("[-] E: {} ".format(e))
+            print("[-] E: {} parse error".format(har))
+    #pool.close()
+    #pool.join()
+            
